47e7793383b43fab6b03a8381a9dcd22fe1fbb18
Archer Heffern <heffernarcher@gmail.com>
2022-07-04 18:21:41 -0400

    Added smooth scroll to heading

---
+++ index.js
@@ -0,0 +1,28 @@
+const welcome = document.getElementById("welcome");
+const aboutMe = document.getElementById("aboutMe");
+const skills = document.getElementById("skills");
+const projects = document.getElementById("projects");
+const welcomeAnchor = document.getElementById("welcomeAnchor")
+const aboutMeAnchor = document.getElementById("aboutMeAnchor")
+const skillsAnchor = document.getElementById("skillsAnchor")
+const projectsAnchor = document.getElementById("projectsAnchor")
+
+welcomeAnchor.addEventListener('click', welcomeScroll)
+aboutMeAnchor.addEventListener('click', aboutMeScroll)
+skillsAnchor.addEventListener('click', skillsScroll)
+projectsAnchor.addEventListener('click', projectsScroll)
+
+function welcomeScroll() {
+    welcome.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+function aboutMeScroll() {
+    aboutMe.scrollIntoView({ behavior: 'smooth', block: 'start' })
+}
+function skillsScroll() {
+    skills.scrollIntoView({ behavior: 'smooth', block: 'start' })
+}
+function projectsScroll() {
+    projects.scrollIntoView({ behavior: 'smooth', block: 'start' })
+}
